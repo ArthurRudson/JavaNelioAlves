b92f8d906397443c438559d7f51494f749b36ca0
ArthurRudson <154820615+ArthurRudson@users.noreply.github.com>
2024-08-15 13:48:58 -0300

    entrada de dados

--- curso_java/src/Main.java
+++ curso_java/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Locale;
 public class Main {
 
 	public static void main(String[] args) {
+		/*
 		int y = 32;
 		double x = 10.35784;
 		System.out.println(y);
@@ -21,7 +22,7 @@ public class Main {
 		double renda = 4000.0;
 		System.out.printf("%s tem %d anos e ganha R$ %2f reais%n", nome, idade, renda);
 		
-		
+		*/
 		
 		
 		// EXERCICIO
@@ -35,7 +36,17 @@ public class Main {
 		double price2 = 650.50;
 		double measure = 53.234567;
 		
-		System.out.printf("Product: %n ", args)
+		System.out.println("Products:");
+		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+		System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+		System.out.println();
+		System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
+		System.out.println();
+		System.out.printf("Measue with eight decimal places: %.8f%n", measure);
+		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f%n", measure);
+		
 		
 	}
 
