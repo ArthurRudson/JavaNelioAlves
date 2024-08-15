744159544ae5ff7f1860efd0bf9cfc2abb31e614
ArthurRudson <154820615+ArthurRudson@users.noreply.github.com>
2024-08-15 09:16:22 -0300

    começo do ex

--- curso_java/src/Main.java
+++ curso_java/src/Main.java
@@ -20,6 +20,23 @@ public class Main {
 		int idade = 31;
 		double renda = 4000.0;
 		System.out.printf("%s tem %d anos e ganha R$ %2f reais%n", nome, idade, renda);
+		
+		
+		
+		
+		// EXERCICIO
+		
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.printf("Product: %n ", args)
+		
 	}
 
 }
