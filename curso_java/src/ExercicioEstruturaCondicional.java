adbf2bf503c1542fcf9991219c255bcd8a9a8c97
ArthurRudson <154820615+ArthurRudson@users.noreply.github.com>
2024-08-18 16:55:31 -0300

    exercicios

---
+++ curso_java/src/ExercicioEstruturaCondicional.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+
+public class ExercicioEstruturaCondicional {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//Exercicio 1
+		//
+		//int a = sc.nextInt();
+		//if (a < 0) {
+		//	System.out.println("NEGATIVO!");
+		//} else {
+		//	System.out.println("NAO NEGATIVO!");
+		//}
+		
+		//Exercicio 2
+		//
+		//int a = sc.nextInt();
+		//if (a % 2 == 0) {
+		//	System.out.println("Par");
+		//} else {
+		//	System.out.println("Impar");
+		//}
+		
+		//Exercicio 3
+		//
+		//int a = sc.nextInt();
+		//int b = sc.nextInt();
+		//if (a % b == 0 || b % a == 0) {
+		//	System.out.println("Sao Multiplos");
+		//} else {
+		//	System.out.println("Nao sao Multiplos");
+		//}
+		
+		//Exercicio 4
+		//
+		//int ini = sc.nextInt();
+		//int fim = sc.nextInt();
+		//int duracao;
+		//if (ini < fim) {
+		//	 duracao = fim - ini;
+		//} else {
+		//	 duracao = 24 - ini + fim;
+		//}
+		//System.out.println("O JOGO DUROU " + duracao + " HORAS(S)");
+		
+		//Exercicio 5
+		//
+		//double hotdog = 4.00;
+		//double xSalada = 4.50;
+		//double xBacon = 5.00;
+		//double torrada = 2.00;
+		//double refri = 1.50;
+		//int codigo = sc.nextInt();
+		//int quantidade = sc.nextInt();
+		//double total;
+		//if (codigo == 1) {
+		//	total = quantidade * hotdog;
+		//	System.out.println("Total: R$ " + total);
+		//} else if (codigo == 2) {
+		//	total = quantidade * xSalada;
+		//	System.out.println("Total: R$ " + total);
+		//} else if (codigo == 3) {
+		//	total = quantidade * xBacon;
+		//	System.out.println("Total: R$ " + total);
+		//} else if (codigo == 4) {
+		//	total = quantidade * torrada;
+		//	System.out.println("Total: R$ " + total);
+		//} else {
+		//	total = quantidade * refri;
+		//	System.out.println("Total: R$ " + total);
+		//}
+		
+		//Exercicio 6
+		//double num = sc.nextDouble();
+		//if (num < 0 || num > 100) {
+		//	System.out.println("Fora de intervalo");
+		//} else if (num <= 25) {
+		//	System.out.println("Intervelo (0,25)");
+		//} else if (num <= 50) {
+		//	System.out.println("Intervalo (25,50)");
+		//} else if (num <= 75) {
+		//	System.out.println("Intervalo (50,75)");
+		//} else if (num <= 100) {
+		//	System.out.println("Intervalo (75,100)");
+		//} else {
+		//	System.out.println();
+		//}
+		
+		//Exercicio 7
+		//
+		//double x = sc.nextDouble();
+		//double y = sc.nextDouble();
+		//if (x > 0 & y > 0) {
+		//	System.out.println("Q1");
+		//} else if (x < 0 & y > 0) {
+		//	System.out.println("Q2");
+		//} else if (x < 0 & y < 0) {
+		//	System.out.println("Q3");
+		//} else if (x > 0 & y < 0) {
+		//	System.out.println("Q4");
+		//} else if (x == 0 & y == 0) {
+		//	System.out.println("Origem");
+		//} else if (x == 0) {
+		//	System.out.println("Eixo Y");
+		//} else if (y == 0) {
+		//	System.out.println("Eixo X");
+		//}
+		
+		sc.close();
+	}
+
+}
--- curso_java/src/Main.java
+++ curso_java/src/Main.java
@@ -1,53 +1,54 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		/*
-		int y = 32;
-		double x = 10.35784;
-		System.out.println(y);
-		System.out.println(x);
-		System.out.printf("%.2f%n", x);
-		System.out.printf("%.4f%n", x);
-		System.out.println("Olá, mundo!");
-		Locale.setDefault(Locale.US);
-		System.out.printf("%.2f%n", x);
-		System.out.printf("%.4f%n", x);
-		System.out.printf("Resultado = %.2f metros%n", x);
-		
-		
-		String nome = "Maria";
-		int idade = 31;
-		double renda = 4000.0;
-		System.out.printf("%s tem %d anos e ganha R$ %2f reais%n", nome, idade, renda);
-		
-		*/
-		
-		
+		 * int y = 32; double x = 10.35784; System.out.println(y);
+		 * System.out.println(x); System.out.printf("%.2f%n", x);
+		 * System.out.printf("%.4f%n", x); System.out.println("Olá, mundo!");
+		 * Locale.setDefault(Locale.US); System.out.printf("%.2f%n", x);
+		 * System.out.printf("%.4f%n", x);
+		 * System.out.printf("Resultado = %.2f metros%n", x);
+		 * 
+		 * 
+		 * String nome = "Maria"; int idade = 31; double renda = 4000.0;
+		 * System.out.printf("%s tem %d anos e ganha R$ %2f reais%n", nome, idade,
+		 * renda);
+		 * 
+		 */
+
 		// EXERCICIO
-		
-		String product1 = "Computer";
-		String product2 = "Office desk";
-		int age = 30;
-		int code = 5290;
-		char gender = 'F';
-		double price1 = 2100.0;
-		double price2 = 650.50;
-		double measure = 53.234567;
-		
-		System.out.println("Products:");
-		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
-		System.out.printf("%s, which price is $ %.2f%n", product2, price2);
-		System.out.println();
-		System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
-		System.out.println();
-		System.out.printf("Measue with eight decimal places: %.8f%n", measure);
-		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+		/*
+		 * String product1 = "Computer"; String product2 = "Office desk"; int age = 30;
+		 * int code = 5290; char gender = 'F'; double price1 = 2100.0; double price2 =
+		 * 650.50; double measure = 53.234567;
+		 * 
+		 * System.out.println("Products:");
+		 * System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+		 * System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+		 * System.out.println();
+		 * System.out.printf("Record: %d years old, code %d and gender: %c%n", age,
+		 * code, gender); System.out.println();
+		 * System.out.printf("Measue with eight decimal places: %.8f%n", measure);
+		 * System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+		 * Locale.setDefault(Locale.US); System.out.printf("US decimal point: %.3f%n",
+		 * measure);
+		 * 
+		 */
+
 		Locale.setDefault(Locale.US);
-		System.out.printf("US decimal point: %.3f%n", measure);
-		
-		
+		Scanner sc = new Scanner(System.in);
+		double largura = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+		double metroQuadrado = sc.nextDouble();
+		double area = largura * comprimento;
+		double preco = area * metroQuadrado;
+		System.out.printf("AREA = %.2f%n", area);
+		System.out.printf("PRECO = %.2f%n", preco);
+		sc.close();
+
 	}
 
 }
