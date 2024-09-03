cfa36965c79f857a1129b85c8105e7007b775549
ArthurRudson <arthurrudson@gmail.com>
2024-09-03 11:29:06 -0300

    exceções

--- Exeception/src/model/entities/Reservation.java
+++ Exeception/src/model/entities/Reservation.java
@@ -1,5 +1,57 @@
 package model.entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Reservation {
 
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation() {
+		
+	}
+
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+	
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+
+	public long durtion() {
+		long diff = getCheckOut().getTime() - getCheckIn().getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MICROSECONDS);
+	}
+	
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn =  checkIn;
+		this.checkOut = checkOut;
+	}
+
+	@Override
+	public String toString() {
+		return "Room " + getRoomNumber() + ", check-in : " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut) + ", " + durtion() + "nights";
+	}
+	
+	
 }
