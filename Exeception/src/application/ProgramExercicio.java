4f559e2afe712c313509e45675030d920184c63f
ArthurRudson <arthurrudson@gmail.com>
2024-09-04 16:26:19 -0300

    começo do exercicio

--- Exeception/src/application/ProgramExercicio.java
+++ Exeception/src/application/ProgramExercicio.java
@@ -1,10 +1,30 @@
 package application;
 
+import java.util.Scanner;
+
 public class ProgramExercicio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int n = sc.nextInt();
+		System.out.print("Holder: ");
+		String holder = sc.next();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdraw = sc.nextDouble();
+		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		System.out.print("New balance: "); 
+		double newBalance = sc.nextDouble();
 
+		sc.close();
 	}
 
 }
--- Exeception/src/model/entities/AccountExercicio.java
+++ Exeception/src/model/entities/AccountExercicio.java
@@ -1,5 +1,62 @@
 package model.entities;
 
 public class AccountExercicio {
+	
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdraw;
+	
+	public AccountExercicio() {
+		
+	}
+
+	public AccountExercicio(Integer number, String holder, Double balance, Double withdraw) {
+		super();
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdraw = withdraw;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public Double getWithdraw() {
+		return withdraw;
+	}
+
+	public void setWithdraw(Double withdraw) {
+		this.withdraw = withdraw;
+	}
+	
+	public void deposit(double amount) {
+		
+	}
+	
+	public void withdraw(double amount) {
+		
+	}
 
 }
