c3929b47f7dfa15670a9285defcc86fc7b08ba7c
ArthurRudson <arthurrudson@gmail.com>
2024-09-24 13:41:53 -0300

    exercicio de fixação

---
+++ FuncionalExpressoesLambda/Exercicio/entities/Employeeee.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Employeeee {
+	
+	private String nome;
+	private String email;
+	private Double salary;
+	
+	public Employeeee(String nome, String email, Double salary) {
+		this.nome = nome;
+		this.email = email;
+		this.salary = salary;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	
+
+}
