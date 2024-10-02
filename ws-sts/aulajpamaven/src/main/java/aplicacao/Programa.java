a0ff354bc95a441c23a9e85395ce0692ea70c3de
ArthurRudson <arthurrudson@gmail.com>
2024-10-02 10:09:52 -0300

    achando erro e terminando o modulo

--- ws-sts/aulajpamaven/src/main/java/aplicacao/Programa.java
+++ ws-sts/aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -11,19 +11,16 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(null, "Carlos da silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana maria", "ana@gmail.com");
-		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
-		
 		EntityManager em = emf.createEntityManager();
+		
+		Pessoa p = em.find(Pessoa.class	, 2);
 		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
+		em.remove(p);
 		em.getTransaction().commit();
 		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 	}
 
 }
